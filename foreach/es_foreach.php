8f178ae7f605821d3d340d85d6d2e0199277b3a7
LucaGiaco <98828116+LucaGiaco@users.noreply.github.com>
2022-02-01 17:11:43 +0100

    Create es_foreach.php

---
+++ foreach/es_foreach.php
@@ -0,0 +1,13 @@
+<?
+$variabile[0] = "0";
+$variabile[1] = "1";
+$variabile[2] = "2";
+$variabile[3] = "3";
+$variabile[4] = "4";
+$variabile[5] = "5";
+$variabile[6] = "6";
+
+foreach($variabile as $valore){
+echo $valore;
+}
+?>
