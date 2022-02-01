54a1745bc4229b072ab86d2df01dbed2e17cb5fd
LucaGiaco <98828116+LucaGiaco@users.noreply.github.com>
2022-02-01 16:50:43 +0100

    Create registro.php

---
+++ registro.php
@@ -0,0 +1,18 @@
+<DOCTYPE html>
+<html>
+<head>
+<title>Registrazione completata</title>
+</head>
+<body>
+
+cognome: <?php echo $_POST["cognome"]; ?> <br>
+nome: <?php echo $_POST["nome"]; ?> <br>
+email: <?php echo $_POST["email"]; ?> <br>
+cellulare: <?php echo $_POST["cell"]; ?> <br>
+username: <?php echo $_POST["user"]; ?> <br>
+password: <?php echo $_POST["passw"]; ?> <br>
+</body>
+</html>
+
+</table>
+</body>
