95eb088e368f42c2ebfc4901636ee237528b5d51
LucaGiaco <98828116+LucaGiaco@users.noreply.github.com>
2022-02-01 17:12:34 +0100

    Create array_associativi.php

---
+++ array_associativi.php
@@ -0,0 +1,11 @@
+<?php
+$calciatore["cognome"] = "Ibrahimovic, ";
+$calciatore["nome"] = "Zlatan";
+print_r($calciatore);
+echo "<br>";
+echo "Cognome: ";
+echo $calciatore["cognome"]." <br />";
+echo "Nome: ";
+echo $calciatore["nome"]." <br />";
+echo "<br>"; 
+?>
