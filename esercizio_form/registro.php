efcf7c6af79f7c973d5fff74298bcf40512c8576
LucaGiaco <98828116+LucaGiaco@users.noreply.github.com>
2022-02-02 10:08:32 +0100

    Update registro.php

--- esercizio_form/registro.php
+++ esercizio_form/registro.php
@@ -11,8 +11,5 @@ email: <?php echo $_POST["email"]; ?> <br>
 cellulare: <?php echo $_POST["cell"]; ?> <br>
 username: <?php echo $_POST["user"]; ?> <br>
 password: <?php echo $_POST["passw"]; ?> <br>
-</body>
-</html>
-
-</table>
-</body>
+  </body>
+  </html>
