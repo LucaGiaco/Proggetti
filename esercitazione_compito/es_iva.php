d4b9a53685512037a5e1317f64d2a229b562230f
LucaGiaco <98828116+LucaGiaco@users.noreply.github.com>
2022-02-08 15:17:37 +0100

    Create es_iva.php

---
+++ esercitazione_compito/es_iva.php
@@ -0,0 +1,22 @@
+<?php
+$arr_arr = array (
+array( "nome"=>"computer", "prezzo"=>750, "quantita"=>3),
+array( "nome"=>"latte", "prezzo"=>2, "quantita"=>6),
+array( "nome"=>"bambino", "prezzo"=>20, "quantita"=>4),
+);
+$prezzoiva = array (0,0,0);
+$numOfRows=count($arr_arr);
+for($i=0; $i<$numOfRows; $i++){
+$row=$arr_arr[$i];
+$nome = $row["nome"];
+$prezzo = $row["prezzo"];
+$quantita = $row["quantita"];
+$prezzotot = $row["prezzo"]*$row["quantita"];
+$prezzoiva[$i] = $prezzotot+$prezzotot*0.22;
+echo "nome del prodotto: ". $row["nome"] . "<br>";
+echo "prezzo base del singolo prodotto: ". $row["prezzo"] . "€<br>";
+echo "quantità: ". $row["quantita"] . "<br>";
+echo "prezzo totale + iva: ". $prezzoiva[$i] . "€<br><br>";
+}
+
+?>
